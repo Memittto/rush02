b0024bff677715cbb4301ca7a83c58b47c3f596c
Stanley SERBIN <sserbin@student.42.fr>
2020-08-22 15:37:54 +0200

    STASH add.c still in progress

---
+++ add.c
@@ -0,0 +1,25 @@
+/*
+	Ft_add_to_it() 
+	On recupere la chaine str;
+	Il faut verifier qu'elle soit au bon format, sinon on return 0 pour erreur;
+	Si c'est ok alors on l'ajout a notre dict
+	Mais le truc c'est qu'il faut aussi gerer quelle dictionnaire
+*/
+
+/*
+	
+*/
+
+int		ft_add_to_dict(char *str)
+{
+	if (ft_verif_add(char *str))
+	{
+		
+	}
+	return (1);
+}
+
+int		ft_verif_add(char *str)
+{
+	return (1);
+}
