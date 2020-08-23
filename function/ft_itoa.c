1f9a47d476db4b66c1791daf21f71a6edb4f2ac4
David PINTO <davpinto@e3r11p20.42.fr>
2020-08-23 14:31:00 +0200

    21

--- function/ft_itoa.c
+++ function/ft_itoa.c
@@ -1,34 +1,52 @@
 #include "../proto.h"
 
-int		size_num(int nb)
+int	nb_len(int nb)
 {
-	int i;
+	int count;
 
-	i = 1;
-	while (nb / 10 > 0)
+	count = 0;
+	if(nb < 0)
 	{
-		nb = nb / 10;
-		i++;
+		nb = nb * -1;
+		count++;
 	}
-	return (i);
+
+	while (nb > 0)
+	{
+		nb = nb /10;
+		count++;
+	}
+	return count;
 }
 
-void	engine(char *str, int n)
+char	*ft_itoa(int nb)
 {
-	if (n / 10 > 0)
-		engine(str - 1, n / 10);
-	*str = (n % 10) + 48;
-}
+	char *str;
+	long  n;
+	int len;
 
-char	*ft_itoa(int n)
-{
-	char	*str;
-	int		length;
+	n = nb;
+	len = nb_len(n);
 
-	length = size_num(n);
-	if (!(str = (char *)malloc(sizeof(char) * length + 1)))
-		return (NULL);
-	str[length] = '\0';
-	engine(str + length - 1, n);
+	str = malloc(sizeof(char) * (len + 1));
+	str[len] = '\0';
+
+	n = nb;
+	if (n == 0)
+	{
+		str[0] = '0';
+		return str;
+	}
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = n * -1;
+	}
+	while (n > 0)
+	{
+		str[len - 1] = 48 + (n % 10);
+		n = n / 10;
+		len--;
+	}
 	return (str);
 }
