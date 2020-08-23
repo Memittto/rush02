b8157deed8eac5000cb6ebedf67386b5834cd2d7
S-Stanley <stanleyserbin@outlook.fr>
2020-08-23 09:35:40 +0200

    ADD copy just in case

---
+++ Stan/proto.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   proto.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sserbin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/22 19:20:44 by sserbin           #+#    #+#             */
+/*   Updated: 2020/08/22 19:23:37 by sserbin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PROTO_H
+# define PROTO_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+int		ft_size_dict(char *str);
+int		ft_verif_add(char *str);
+int		ft_strlen(char *str);
+int		ft_add_to_dict(char *str);
+int		ft_strcmp(char *s1, char *s2);
+void	ft_putstr(int fd, char *str);
+
+#endif
